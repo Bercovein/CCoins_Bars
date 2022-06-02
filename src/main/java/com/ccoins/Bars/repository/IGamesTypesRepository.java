8b06b8a21413e7812464c8e95c39012101ef11b8
Nicolas Bercovsky <bercovsky.nm@gmail.com>
2022-06-02 20:15:44 -0300

    games ABM

--- src/main/java/com/ccoins/Bars/repository/IGamesTypesRepository.java
+++ src/main/java/com/ccoins/Bars/repository/IGamesTypesRepository.java
@@ -1,16 +1,14 @@
 package com.ccoins.Bars.repository;
 
-import com.ccoins.Bars.model.Game;
 import com.ccoins.Bars.model.GameType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface IGamesTypesRepository extends JpaRepository<GameType, Long> {
 
-    Optional<List<Game>> findAll(Long id);
+    List<GameType> findAll();
 
 }

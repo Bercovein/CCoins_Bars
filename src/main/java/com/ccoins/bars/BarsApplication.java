f4158a7c6944c659791e56bfa13a2e0bde8e30a3
Nicolas Bercovsky <bercovsky.nm@gmail.com>
2023-07-13 00:47:04 -0300

    CorsFilter

--- src/main/java/com/ccoins/bars/BarsApplication.java
+++ src/main/java/com/ccoins/bars/BarsApplication.java
@@ -3,6 +3,7 @@ package com.ccoins.bars;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableFeignClients
 @EnableScheduling
+@ServletComponentScan
 public class BarsApplication
 		extends SpringBootServletInitializer
 {
